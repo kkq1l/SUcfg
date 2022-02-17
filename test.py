8e56ec57feac7c51b6e564106a602cea19129e26
kkq1l <d.mmr@inbox.ru>
2022-02-18 00:24:21 +0900

    search steam

--- test.py
+++ test.py
@@ -8,3 +8,30 @@ ID3 = (((y - x) // 2) * 2) + x
 
 print(ID3)
 
+impsys
+impwinreg
+>>>
+>>>
+try:
+h= winreg.OpenKey(winreg.HKEY_LOCAL_MACHI"SOFTWARE\WOW6432Node\Valve\Steam")
+except:
+h= None
+print(sys.exc_info())
+...
+hkey
+<PyHobjat 0x00000154FF5D0390>
+>>>
+try:
+steam_p= winreg.QueryValueEx(hk"InstallPath")
+except:
+steam_p= None
+print(sys.exc_info())
+...
+steam_path
+('C:\\ProgFi(x86)\\Stea1)
+steam_path[0]
+'C:\\ProgFi(x86)\\Steam'
+steam_path== winreg.REG_SZ
+True
+>>>
+winreg.CloseKey(hkey)
