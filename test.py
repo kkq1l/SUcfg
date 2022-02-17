fe9fe6b91acac5e37a30b3bc00563f97b4219d8b
kkq1l <d.mmr@inbox.ru>
2022-02-18 01:30:12 +0900

    add search steam location, copy u cfg

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-#converting steam id64 to id3
-ID64 = 76561198133783003
+#converting steam id
+ID64 = 76561198136787000
 
 y = int(ID64) - 76561197960265728
 x = y % 2
@@ -8,30 +8,19 @@ ID3 = (((y - x) // 2) * 2) + x
 
 print(ID3)
 
-impsys
-impwinreg
->>>
->>>
-try:
-h= winreg.OpenKey(winreg.HKEY_LOCAL_MACHI"SOFTWARE\WOW6432Node\Valve\Steam")
-except:
-h= None
-print(sys.exc_info())
-...
-hkey
-<PyHobjat 0x00000154FF5D0390>
->>>
-try:
-steam_p= winreg.QueryValueEx(hk"InstallPath")
-except:
-steam_p= None
-print(sys.exc_info())
-...
-steam_path
-('C:\\ProgFi(x86)\\Stea1)
-steam_path[0]
-'C:\\ProgFi(x86)\\Steam'
-steam_path== winreg.REG_SZ
-True
->>>
-winreg.CloseKey(hkey)
+
+#where installing steam
+import winreg
+from winreg import *
+
+aKey = r"SOFTWARE\WOW6432Node\Valve\Steam"
+aReg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+aKey = OpenKey(aReg, aKey)
+steam_path = winreg.QueryValueEx(aKey, "InstallPath")
+path=steam_path[0]
+print((steam_path[0]))
+
+#copy u cfg
+import shutil
+path = path+r'\userdata/'+str(ID3)+'/730/local/cfg/config.cfg'
+shutil.copyfile(path,"myCFG.cfg")
