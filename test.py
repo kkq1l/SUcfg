a2ee87640f5de1f9b7ec8197e4e977918a17e57d
kkq1l <d.mmr@inbox.ru>
2022-02-18 22:50:36 +0900

    regedit

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #converting steam id
-ID64 = 76561198136787000
+ID64 = 76561198133783003
 
 y = int(ID64) - 76561197960265728
 x = y % 2
@@ -13,14 +13,14 @@ print(ID3)
 import winreg
 from winreg import *
 
-aKey = r"SOFTWARE\WOW6432Node\Valve\Steam"
-aReg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+aKey = r"SOFTWARE\Valve\Steam"
+aReg = ConnectRegistry(None, HKEY_CURRENT_USER)
 aKey = OpenKey(aReg, aKey)
-steam_path = winreg.QueryValueEx(aKey, "InstallPath")
+steam_path = winreg.QueryValueEx(aKey, "SteamPath")
 path=steam_path[0]
 print((steam_path[0]))
 
 #copy u cfg
 import shutil
 path = path+r'\userdata/'+str(ID3)+'/730/local/cfg/config.cfg'
-shutil.copyfile(path,"myCFG.cfg")
+shutil.copyfile(path,"asd.cfg")
